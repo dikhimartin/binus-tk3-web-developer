8cecbcc15fcfbd1b5f0a49ab2afe91f0764c42da
Dikhi Martin <dikhi.martin@gmail.com>
2023-05-04 12:53:48 +0700

    autocommit

--- project/resources/views/backend/home.blade.php
+++ project/resources/views/backend/home.blade.php
@@ -62,72 +62,15 @@
                             
                             <div class="d-flex flex-stack bg-success rounded-3 p-6 mb-11">
                                 <div class="fs-6 fw-bold text-white">
-                                    <span class="d-block lh-1 mb-2">Subtotal</span>
-                                    <span class="d-block mb-2">Discounts</span>
-                                    <span class="d-block mb-9">Tax(12%)</span>
                                     <span class="d-block fs-2qx lh-1">Total</span>
                                 </div>
                                 <div class="fs-6 fw-bold text-white text-end">
-                                    <span class="d-block lh-1 mb-2" data-kt-pos-element="total">$100.50</span>
-                                    <span class="d-block mb-2" data-kt-pos-element="discount">-$8.00</span>
-                                    <span class="d-block mb-9" data-kt-pos-element="tax">$11.20</span>
-                                    <span class="d-block fs-2qx lh-1" data-kt-pos-element="grant-total">$93.46</span>
+                                    <span class="d-block fs-2qx lh-1" data-kt-pos-element="grant-total">Rp 16.000</span>
                                 </div>
                             </div>
-
-
                             <div class="m-0">
-                                <!--begin::Title-->
-                                <h1 class="fw-bold text-gray-800 mb-5">Payment Method</h1>
-                                <!--end::Title-->
-                                <!--begin::Radio group-->
-                                <div class="d-flex flex-equal gap-5 gap-xxl-9 px-0 mb-12" data-kt-buttons="true" data-kt-buttons-target="[data-kt-button]">
-                                    <!--begin::Radio-->
-                                    <label class="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4" data-kt-button="true">
-                                        <!--begin::Input-->
-                                        <input class="btn-check" type="radio" name="method" value="0" />
-                                        <!--end::Input-->
-                                        <!--begin::Icon-->
-                                        <i class="fonticon-cash-payment fs-2hx mb-2 pe-0"></i>
-                                        <!--end::Icon-->
-                                        <!--begin::Title-->
-                                        <span class="fs-7 fw-bold d-block">Cash</span>
-                                        <!--end::Title-->
-                                    </label>
-                                    <!--end::Radio-->
-                                    <!--begin::Radio-->
-                                    <label class="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4 active" data-kt-button="true">
-                                        <!--begin::Input-->
-                                        <input class="btn-check" type="radio" name="method" value="1" />
-                                        <!--end::Input-->
-                                        <!--begin::Icon-->
-                                        <i class="fonticon-card fs-2hx mb-2 pe-0"></i>
-                                        <!--end::Icon-->
-                                        <!--begin::Title-->
-                                        <span class="fs-7 fw-bold d-block">Card</span>
-                                        <!--end::Title-->
-                                    </label>
-                                    <!--end::Radio-->
-                                    <!--begin::Radio-->
-                                    <label class="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4" data-kt-button="true">
-                                        <!--begin::Input-->
-                                        <input class="btn-check" type="radio" name="method" value="2" />
-                                        <!--end::Input-->
-                                        <!--begin::Icon-->
-                                        <i class="fonticon-mobile-payment fs-2hx mb-2 pe-0"></i>
-                                        <!--end::Icon-->
-                                        <!--begin::Title-->
-                                        <span class="fs-7 fw-bold d-block">E-Wallet</span>
-                                        <!--end::Title-->
-                                    </label>
-                                    <!--end::Radio-->
-                                </div>
-                                <!--end::Radio group-->
-                                <!--begin::Actions-->
-                                <button class="btn btn-primary fs-1 w-100 py-4">Print Bills</button>
-                                <!--end::Actions-->
+                                <button class="btn btn-primary fs-1 w-100 py-4">Pesan</button>
                             </div>
-                            <!--end::Payment Method-->
                         </div>
                     </div>
                 </div>
