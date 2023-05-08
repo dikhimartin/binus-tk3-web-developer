0171c11fb1a32f4ac086d33d482964b7a388e17f
Dikhi Martin <dikhi.martin@gmail.com>
2023-05-08 19:59:30 +0700

    autocommit

--- project/resources/views/backend/shared/sidebar.blade.php
+++ project/resources/views/backend/shared/sidebar.blade.php
@@ -32,7 +32,6 @@
         <div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
             <!--begin::Menu-->
             <div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
-                <!--begin:Menu item-->
                 <div class="menu-item">
                     <a class="menu-link {!! $sidebarActive == 'home' ? ' active' : '' !!}" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( 'admin/dashboard' ))}}">
                         <span class="menu-icon">
@@ -42,56 +41,7 @@
                     </a>
                 </div>
 
-                <!--begin:Menu item-->
-                @if ($user->can('transaction-list'))
-                    <div class="menu-item">
-                        <a class="menu-link {!! $sidebarActive == 'transaction' ? ' active' : '' !!}" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( 'admin/transaction' ))}}">
-                            <span class="menu-icon">
-                                <i class="fa fa-table"></i>
-                            </span>
-                            <span class="menu-title">{{ __('main.transaction') }}</span>
-                        </a>
-                    </div>
-                @endif
-
-
-                @if ($user->can('report-list'))
-                    <div data-kt-menu-trigger="click" class="menu-item {{ in_array($sidebarActive, ['transaction-report','product-report']) ? 'here show menu-accordion' : '' }}">
-                        <!--begin:Menu link-->
-                        <span class="menu-link ">
-                            <span class="menu-icon">
-                                <i class="fa-solid fa-server"></i>
-                            </span>
-                            <span class="menu-title">{{__('main.report')}}</span>
-                            <span class="menu-arrow"></span>
-                        </span>
-                        <!--begin:Menu sub-->
-                        <div class="menu-sub menu-sub-accordion">
-                            <div class="menu-item">
-                                <a class="menu-link {!! $sidebarActive == 'transaction-report' ? ' active' : '' !!}" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( 'admin/report/transaction' ))}}">
-                                    <span class="menu-bullet">
-                                        <span class="bullet bullet-dot"></span>
-                                    </span>
-                                    <span class="menu-title">{{ __('main.transaction-report') }}</span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="menu-sub menu-sub-accordion">
-                            <div class="menu-item">
-                                <a class="menu-link {!! $sidebarActive == 'product-report' ? ' active' : '' !!}" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( 'admin/report/product' ))}}">
-                                    <span class="menu-bullet">
-                                        <span class="bullet bullet-dot"></span>
-                                    </span>
-                                    <span class="menu-title">{{ __('main.product-report') }}</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>                
-                 @endif
-
-                
-
-                 @if ($user->can('product-type-list') || $user->can('product-list') || $user->can('customer-list') || $user->can('staff-list') )
+                @if ($user->can('product-type-list') || $user->can('product-list') || $user->can('customer-list') || $user->can('staff-list') )
                     <div data-kt-menu-trigger="click" class="menu-item {{ in_array($sidebarActive, ['product','product-type','customer', 'staff']) ? 'here show menu-accordion' : '' }}">
                         <!--begin:Menu link-->
                         <span class="menu-link ">
@@ -154,9 +104,54 @@
                         @endif
 
                     </div>                
-                 @endif
+                @endif
 
-                 @if ($user->can('users-list') || $user->can('roles-list') || $user->can('group_user-list') )
+                @if ($user->can('transaction-list'))
+                    <div class="menu-item">
+                        <a class="menu-link {!! $sidebarActive == 'transaction' ? ' active' : '' !!}" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( 'admin/transaction' ))}}">
+                            <span class="menu-icon">
+                                <i class="fa fa-table"></i>
+                            </span>
+                            <span class="menu-title">{{ __('main.transaction') }}</span>
+                        </a>
+                    </div>
+                @endif
+
+                @if ($user->can('report-list'))
+                    <div data-kt-menu-trigger="click" class="menu-item {{ in_array($sidebarActive, ['transaction-report','product-report']) ? 'here show menu-accordion' : '' }}">
+                        <!--begin:Menu link-->
+                        <span class="menu-link ">
+                            <span class="menu-icon">
+                                <i class="fa-solid fa-chart-pie"></i>
+                            </span>
+                            <span class="menu-title">{{__('main.report')}}</span>
+                            <span class="menu-arrow"></span>
+                        </span>
+                        <!--begin:Menu sub-->
+                        <div class="menu-sub menu-sub-accordion">
+                            <div class="menu-item">
+                                <a class="menu-link {!! $sidebarActive == 'transaction-report' ? ' active' : '' !!}" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( 'admin/report/transaction' ))}}">
+                                    <span class="menu-bullet">
+                                        <span class="bullet bullet-dot"></span>
+                                    </span>
+                                    <span class="menu-title">{{ __('main.transaction-report') }}</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="menu-sub menu-sub-accordion">
+                            <div class="menu-item">
+                                <a class="menu-link {!! $sidebarActive == 'product-report' ? ' active' : '' !!}" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( 'admin/report/product' ))}}">
+                                    <span class="menu-bullet">
+                                        <span class="bullet bullet-dot"></span>
+                                    </span>
+                                    <span class="menu-title">{{ __('main.product-report') }}</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>                
+                @endif
+
+                @if ($user->can('users-list') || $user->can('roles-list') || $user->can('group_user-list') )
                     <div data-kt-menu-trigger="click" class="menu-item {{ in_array($sidebarActive, ['users', 'group_user', 'roles']) ? 'here show menu-accordion' : '' }}">
                         <!--begin:Menu link-->
                         <span class="menu-link ">
@@ -193,7 +188,7 @@
                         @endif
 
                     </div>                
-                 @endif
+                @endif
             </div>
         </div>
     </div>
